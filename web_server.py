0def0215a707429cce1912d5d8a7a429f073f2d7
Angelo Santangelo <asantangelo11@gmail.com>
2024-06-24 00:14:38 +0200

    Query 1 done!

--- web_server.py
+++ web_server.py
@@ -10,12 +10,17 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return redirect(url_for('retrieve'))
+    return render_template('index.html')
 
-@app.route('/retrieve')
-def retrieve():
-    partecipanti = mongo.db.partecipanti.find().limit(20)
-    return render_template('sample.html', partecipanti=partecipanti)
+@app.route('/welcome')
+def welcome():
+    return render_template('index.html')
+    
+
+@app.route('/retrieveEventiWithArtisti')
+def retrieveEventiWithArtisti():
+    eventi = mongo.db.eventi.find()
+    return render_template('query1.html', eventi=eventi)
 
 if __name__ == '__main__':
     app.secret_key = 'secretivekey'
