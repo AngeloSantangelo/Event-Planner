9a5585b4445c2f9fa42927b12b46bcda38ae8db8
Angelo Santangelo <asantangelo11@gmail.com>
2024-06-26 23:25:40 +0200

    Query 5 - 6 - 9 implemented

--- web_server.py
+++ web_server.py
@@ -19,48 +19,46 @@ def welcome():
     return render_template('index.html')
     
 
-@app.route('/retrieveEventiWithArtisti')
-def retrieveEventiWithArtisti():
+@app.route('/query1')
+def query1():
     eventi = mongo.db.eventi.find()
     return render_template('query1.html', eventi=eventi)
 
-@app.route('/add_event', methods=['POST'])
-def add_event():
-    eventi = mongo.db.eventi
-    tema = request.form.get('tema')
-    data = request.form.get('data')
-    prezzo_biglietto = request.form.get('prezzo_biglietto')
-    nome_locale = request.form.get('nome_locale')
-    artisti_data = request.form.getlist('artisti')
-    try:
-        # Converting date from GG/MM/AAAA to datetime object
-        data = datetime.strptime(data, '%d/%m/%Y').strftime('%d/%m/%Y')
-    except ValueError:
-        return "Invalid date format. Please use GG/MM/AAAA."
-    artisti_details = []
-    for artista in artisti_data:
-        artista_id, nome_arte, tipo, telefono, prezzo_artista = artista.split('|')
-        artisti_details.append({
-            '_id': ObjectId(artista_id),
-            'nome_arte': nome_arte,
-            'tipo': tipo,
-            'telefono': telefono,
-            'prezzo_artista': int(prezzo_artista)
-        })
-    evento = {
-        'tema': tema,
-        'data': data,
-        'prezzo_biglietto': prezzo_biglietto,
-        'nome_locale': nome_locale,
-        'artisti': artisti_details
-    }
-    eventi.insert_one(evento)
-    return render_template('index.html')
-
-@app.route('/query2')
+@app.route('/query2', methods=['GET', 'POST'])
 def query2():
-    artisti = mongo.db.artisti.find()
-    return render_template('query2.html', artisti=artisti)
+    if request.method == 'POST':
+        eventi = mongo.db.eventi
+        tema = request.form.get('tema')
+        data = request.form.get('data')
+        prezzo_biglietto = request.form.get('prezzo_biglietto')
+        nome_locale = request.form.get('nome_locale')
+        artisti_data = request.form.getlist('artisti')
+        try:
+            data = datetime.strptime(data, '%d/%m/%Y').strftime('%d/%m/%Y')
+        except ValueError:
+            return "Invalid date format. Please use GG/MM/AAAA."
+        artisti_details = []
+        for artista in artisti_data:
+            artista_id, nome_arte, tipo, telefono, prezzo_artista = artista.split('|')
+            artisti_details.append({
+                '_id': ObjectId(artista_id),
+                'nome_arte': nome_arte,
+                'tipo': tipo,
+                'telefono': telefono,
+                'prezzo_artista': int(prezzo_artista)
+            })
+        evento = {
+            'tema': tema,
+            'data': data,
+            'prezzo_biglietto': prezzo_biglietto,
+            'nome_locale': nome_locale,
+            'artisti': artisti_details
+        }
+        eventi.insert_one(evento)
+        return render_template('index.html')
+    else:
+        artisti = mongo.db.artisti.find()
+        return render_template('query2.html', artisti=artisti)
 
 @app.route('/query3')
 def query3():
@@ -95,6 +93,58 @@ def query4():
 
     return render_template('query4.html', eventi=risultati)
 
+@app.route('/query5', methods=['GET', 'POST'])
+def query5():
+    if request.method == 'POST':
+        data_inizio = request.form['data_inizio']
+        data_fine = request.form['data_fine']
+
+        # Conversione delle date in oggetti datetime
+        data_inizio_dt = datetime.strptime(data_inizio, '%d/%m/%Y')
+        data_fine_dt = datetime.strptime(data_fine, '%d/%m/%Y')
+
+        # Trovare gli eventi che si svolgono tra le date specificate
+        eventi = []
+        for evento in mongo.db.eventi.find():
+            data_evento_str = evento.get('data', '')
+            if data_evento_str:
+                data_evento_dt = datetime.strptime(data_evento_str, '%d/%m/%Y')
+                if data_inizio_dt <= data_evento_dt <= data_fine_dt:
+                    eventi.append(evento)
+
+        return render_template('query5.html', eventi=eventi, data_inizio=data_inizio, data_fine=data_fine)
+    
+    return render_template('query5_form.html')
+
+
+@app.route('/query6', methods=['GET', 'POST'])
+def query6():
+    if request.method == 'POST':
+        budget = float(request.form['prezzo'])
+
+        # Trovare gli artisti che hanno un budget al di sotto di un determinato prezzo
+        artisti = []
+        for artista in mongo.db.artisti.find():
+            prezzo_artista = float(artista.get('prezzo_artista', ''))
+            if prezzo_artista <= budget:
+                artisti.append(artista)
+        return render_template('query6.html', artisti=artisti, budget=budget)
+    return render_template('query6_form.html')
+
+
+@app.route('/query9', methods=['GET', 'POST'])
+def query9():
+    if request.method == 'POST':
+        locale = request.form['locale']
+
+        # Trovare gli artisti che hanno un budget al di sotto di un determinato prezzo
+        eventi = []
+        for evento in mongo.db.eventi.find():
+            nome_locale = evento.get('nome_locale', '')
+            if locale == nome_locale:
+                eventi.append(evento)
+        return render_template('query9.html', eventi=eventi, locale=locale)
+    return render_template('query9_form.html')
 
 @app.route('/query10')
 def query10():
